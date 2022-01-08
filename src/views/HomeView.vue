03fbdb3ac90f2b9de817271b5d53c0dca86da48c
Loris Polenz <contact@lorispolenz.com>
2022-01-08 02:46:28 +0100

    added default structure and style imports to views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<style lang="scss" scoped src="@/styles/views/HomeView.scss"></style>
+
+<template>
+  <ion-page> </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage } from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+  },
+};
+</script>
---
+++ src/views/TripSavedView.vue
@@ -0,0 +1,17 @@
+<style lang="scss" scoped src="@/styles/views/TripSavedView.scss"></style>
+
+<template>
+    <ion-page>
+        
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage } from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+  },
+};
+</script>
