be7cf5f30ee5d19f75ca98dd5f2e3f7e76f7b921
Marvin Gabriel <m.gabriel@rafisa.ch>
2022-01-11 16:47:51 +0100

    worked on train proposal card

--- src/views/TripProposalView.vue
+++ src/views/TripProposalView.vue
@@ -2,23 +2,25 @@
 
 <template>
     <ion-page>
+      <ion-list>
         <ion-card>
           <ion-card-content>
-              <ion-row id="proposalRow1">
+              <ion-row class="proposalRow1">
                 <ion-badge color="danger">S 11</ion-badge>
                 <ion-text>Richtung, <b>Aarau</b></ion-text>
               </ion-row>
-              <ion-row id="proposalRow2">
+              <ion-row class="proposalRow2">
                 <ion-text><b>15:09</b></ion-text>
-                <div id="proposalLine"><hr><i class='bx bx-right-arrow-alt'></i></div>
+                  <div><i class='bx bxs-left-arrow'></i><hr><i class='bx bxs-right-arrow'></i></div>
                 <ion-text><b>15:28</b></ion-text>
               </ion-row>
-              <ion-row id="proposalRow2">
+              <ion-row class="proposalRow2">
                 <ion-text>Winterthur, HB</ion-text>
                 <ion-text>Zürich, HB</ion-text>
               </ion-row>
           </ion-card-content>
         </ion-card>
+      </ion-list>
     </ion-page>
 </template>
 
