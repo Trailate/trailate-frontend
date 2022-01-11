5ab1b27fc23bd1d25d206687750391847f156772
Marvin Gabriel <m.gabriel@rafisa.ch>
2022-01-11 16:13:45 +0100

    started workinng on train proposal card

--- src/views/TripProposalView.vue
+++ src/views/TripProposalView.vue
@@ -2,12 +2,29 @@
 
 <template>
     <ion-page>
-        
+        <ion-card>
+          <ion-card-content>
+              <ion-row id="proposalRow1">
+                <ion-badge color="danger">S 11</ion-badge>
+                <ion-text>Richtung, <b>Aarau</b></ion-text>
+              </ion-row>
+              <ion-row id="proposalRow2">
+                <ion-text><b>15:09</b></ion-text>
+                <div id="proposalLine"><hr><i class='bx bx-right-arrow-alt'></i></div>
+                <ion-text><b>15:28</b></ion-text>
+              </ion-row>
+              <ion-row id="proposalRow2">
+                <ion-text>Winterthur, HB</ion-text>
+                <ion-text>Zürich, HB</ion-text>
+              </ion-row>
+          </ion-card-content>
+        </ion-card>
     </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage } from "@ionic/vue";
+import 'boxicons';
 
 export default {
   components: {
