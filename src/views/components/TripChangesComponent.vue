c78fba36543fda8f6e34c0c561a650e91a712d4c
Marvin Gabriel <m.gabriel@rafisa.ch>
2022-02-01 14:43:31 +0100

    implemented modals for wochentage, message, contacts

--- src/views/components/TripChangesComponent.vue
+++ src/views/components/TripChangesComponent.vue
@@ -14,15 +14,15 @@
                 <ion-text>Wochentage</ion-text>
                 <ion-button color="danger" id="modalWeekDaysTrigger"><i class='bx bxs-time'></i></ion-button>
             </ion-row>
-            
-            <ion-row>
-                <ion-text>Kontakte</ion-text> 
-                <ion-button color="danger"><i class='bx bxs-contact'></i></ion-button>
-            </ion-row>
 
             <ion-row>
                 <ion-text>Nachricht</ion-text> 
-                <ion-button color="danger"><i class='bx bxs-pencil'></i></ion-button>
+                <ion-button color="danger" id="modalMessageTrigger"><i class='bx bxs-pencil'></i></ion-button>
+            </ion-row>
+            
+            <ion-row>
+                <ion-text>Kontakte</ion-text> 
+                <ion-button color="danger" id="modalContactTrigger"><i class='bx bxs-contact'></i></ion-button>
             </ion-row>
 
             <ion-row>
@@ -35,21 +35,77 @@
           </ion-card-content>
         </ion-card>
 
-        <ion-modal trigger="modalWeekDaysTrigger" id="modalWeekdays" css-class="modalWeekdays" :is-open="false" backdrop-dismiss="true"   :initialBreakpoint="0.16">
+        <ion-modal trigger="modalWeekDaysTrigger" :is-open="false" backdrop-dismiss="true"   :initialBreakpoint="0.48">
             <ion-content>
-                <ion-row id="modalRow1">
-                    <ion-button color="medium" id="weekdayMonday" strong="true">Mo</ion-button>
-                    <ion-button color="medium" id="weekdayTuesday" strong="true">Di</ion-button>
-                    <ion-button color="medium" id="weekdayWednesday" strong="true">Mi</ion-button>
-                    <ion-button color="medium" id="weekdayThursday" strong="true">Do</ion-button>
-                </ion-row>
-                <ion-row>
-                    <ion-button color="medium" id="weekdayFriday" strong="true">Fr</ion-button>
-                    <ion-button color="medium" id="weekdaySaturday" strong="true">Sa</ion-button>
-                    <ion-button color="medium" id="weekdaySunday" strong="true">So</ion-button>
-                </ion-row>
+                <div>
+                    <h3>Wochentage</h3>
+                    <ion-row class="weekday">
+                        <ion-text>Montag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Dienstag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Mittwoch</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Donnerstag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Freitag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Samstag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-row class="weekday">
+                        <ion-text>Sonntag</ion-text>
+                        <ion-checkbox color="danger"></ion-checkbox>
+                    </ion-row>
+                    <ion-button color="danger" expand="block">Save Changes</ion-button>
+                </div>
+
             </ion-content>
         </ion-modal>
+
+        <ion-modal trigger="modalMessageTrigger" :is-open="false" backdrop-dismiss="true" :initialBreakpoint="0.32">
+            <ion-content>
+                <div>
+
+                    <h3>Nachricht</h3>
+                    <ion-textarea placeholder="placeholder" rows="6" cols="20" background-color="medium"></ion-textarea>
+                    <ion-button color="danger" expand="block">Save Changes</ion-button>
+                
+                </div>
+            </ion-content>
+        </ion-modal>
+
+        <ion-modal trigger="modalContactTrigger" :is-open="false" backdrop-dismiss="true" :initialBreakpoint="0.32">
+            <ion-content>
+                <div>
+
+                    <h3>Kontakte</h3>
+                    <ion-row id="addContact">
+                        <ion-input inputmode="email"></ion-input>
+                        <ion-button color="danger"><i class='bx bx-plus-medical' ></i></ion-button>
+                    </ion-row>
+                    <ion-row id="listContacts">
+                        <ion-text>example@gmail.com</ion-text>
+                        <ion-text>example@gmail.com</ion-text>
+                        <ion-text>example@gmail.com</ion-text>
+                        <ion-text>example@gmail.com</ion-text>
+                        <ion-text>example@gmail.com</ion-text>
+                    </ion-row>
+
+                </div>
+            </ion-content>
+        </ion-modal>
+
 </template>
 
 <script>
