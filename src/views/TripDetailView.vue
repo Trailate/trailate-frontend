30cf5acabc0fd7dcf4c8a9decbfce93b441f229c
Marvin Gabriel <m.gabriel@rafisa.ch>
2022-02-01 12:06:10 +0100

    tons of frontend changes

--- src/views/TripDetailView.vue
+++ src/views/TripDetailView.vue
@@ -1,17 +1,40 @@
 <style lang="scss" scoped src="@/styles/views/TripDetailView.scss"></style>
 
 <template>
-    <ion-page>
-        
-    </ion-page>
+
+  <ion-header translucent>
+    <ion-toolbar>
+      <ion-title>Edit Trip</ion-title>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-page>
+
+    <ion-content>
+      <ion-list id="list">
+        <trip-component></trip-component>
+        <trip-changes-component></trip-changes-component>
+      </ion-list>
+
+    </ion-content>
+
+  </ion-page>
+
 </template>
+
 
 <script lang="ts">
 import { IonPage } from "@ionic/vue";
+import TripComponent from './components/TripComponent.vue';
+import TripChangesComponent from './components/TripChangesComponent.vue';
 
 export default {
   components: {
     IonPage,
+    TripComponent,
+    TripChangesComponent,
   },
 };
 </script>
+
+    TripChangesComponent
--- src/views/TripSavedView.vue
+++ src/views/TripSavedView.vue
@@ -1,17 +1,43 @@
 <style lang="scss" scoped src="@/styles/views/TripSavedView.scss"></style>
 
 <template>
-    <ion-page>
-        
-    </ion-page>
+
+  <ion-header translucent>
+    <ion-toolbar>
+      <ion-title>My Trips</ion-title>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-page>
+
+    <ion-content>
+
+      <ion-list id="list">
+        <trip-component href="/trip/1"></trip-component>
+        <trip-component href="/trip/2"></trip-component>
+        <trip-component href="/trip/3"></trip-component>
+      </ion-list>
+
+      <ion-infinite-scroll threshold="100px" id="infinite-scroll">
+        <ion-infinite-scroll-content loading-spinner="bubbles" loading-text="Loading more data...">
+        </ion-infinite-scroll-content>
+      </ion-infinite-scroll>  
+
+    </ion-content>
+
+  </ion-page>
+
 </template>
 
 <script lang="ts">
 import { IonPage } from "@ionic/vue";
+import TripComponent from './components/TripComponent.vue';
+import 'boxicons';
 
 export default {
   components: {
     IonPage,
+    TripComponent,
   },
 };
 </script>
