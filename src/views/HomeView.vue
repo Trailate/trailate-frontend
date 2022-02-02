b49333ae06def2bfeaf4598baffa9556a588d906
MDS160902 <michael.desmitt@edu.tbz.ch>
2022-02-02 21:58:21 +0100

    Worked on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,37 +2,45 @@
 
 <template>
   <ion-page>
-    <div class="searchitem-section">
-      <!-- <ion-searchbar
-        color="light"
-        class="searchbar-from"
-        placeholder="From"
-      ></ion-searchbar>
-      <ion-searchbar
-        color="light"
-        class="searchbar-to"
-        placeholder="To"
-      ></ion-searchbar> -->
+    <div class="grid-container">
+      <div class="grid-item">
+        <h1>Welcome to Trailate</h1>
+      </div>
+      <div class="grid-item">
+        <div class="searchitem-section">
+          <!-- <ion-searchbar
+            color="light"
+            class="searchbar-from"
+            placeholder="From"
+          ></ion-searchbar>
+          <ion-searchbar
+            color="light"
+            class="searchbar-to"
+            placeholder="To"
+          ></ion-searchbar> -->
+          <ion-item class="item-from">
+            <ion-label position="floating">From</ion-label>
+            <ion-input></ion-input>
+          </ion-item>
 
-      <ion-item class="item-from">
-        <ion-label position="floating">From</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
+          <ion-item class="item-to">
+            <ion-label position="floating">To</ion-label>
+            <ion-input></ion-input>
+          </ion-item>
 
-      <ion-item class="item-to">
-        <ion-label position="floating">To</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
-
-      <div id="app">
-        {{ info }}
+          <div id="app">
+            {{ info }}
+          </div>
+        </div>
       </div>
     </div>
+
+
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonItem, IonLabel, IonInput,  } from "@ionic/vue";
+import { IonPage, IonItem, IonLabel, IonInput, } from "@ionic/vue";
 import axios from "axios";
 
 export default {
@@ -70,4 +78,21 @@ export default {
   left: 50%;
   transform: translate(-50%, -50%);
 }
+
+.grid-container {
+  display: grid;
+  grid-template-columns: auto;
+  background-color: #262626;
+  padding: 5%;
+  margin: 5%;
+}
+
+.grid-item {
+  padding: 1%;
+  margin: 1%;
+  font-size: 20vw;
+  text-align: center;
+  font-weight: bold;
+}
+
 </style>
